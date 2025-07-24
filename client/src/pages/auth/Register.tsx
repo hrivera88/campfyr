c5582fac7cfaebdf6abe65cf4954f7f51c1a58e0
hrivera88 <hrivera@outlook.com>
2025-07-23 22:22:30 -0500

    Updated AuthPageLayout copy

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -1,24 +1,24 @@
-import { Box, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import RegisterForm from "../../components/forms/RegisterForm";
-import AuthPageLayout from "../../layouts/AuthPageLayout";
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import RegisterForm from '../../components/forms/RegisterForm';
+import AuthPageLayout from '../../layouts/AuthPageLayout';
 
-const Register = () => { 
+const Register = () => {
   const navigate = useNavigate();
 
   return (
     <AuthPageLayout
-      title="Join in with"
+      title="Join the camp "
       showLogo={true}
       maxWidth={480}
       iconTransformY="22%"
     >
       <RegisterForm />
-      
-      <Box mt={2} textAlign={"center"}>
-        <Button 
-          variant="text" 
-          onClick={() => navigate("/login")}
+
+      <Box mt={2} textAlign={'center'}>
+        <Button
+          variant="text"
+          onClick={() => navigate('/login')}
           sx={{ textTransform: 'none' }}
         >
           Have an account? Sign In
